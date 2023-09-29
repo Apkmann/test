a9b566cafc45a471ab686016760d89f39a6c4da0
Mohammed Aswath L <102312905+Apkmann@users.noreply.github.com>
2023-09-29 15:46:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask ,request,render_template,redirect,url_for
+from pymongo import MongoClient
+import datetime as dt
+import urllib.parse
+passw = "Aswath22@data"
+p = urllib.parse.quote(passw)
+app = Flask(__name__,template_folder='template',static_url_path="/static")
+cl = MongoClient(f'mongodb+srv://mohammedaswath141:{p}@cluster0.54i4ysr.mongodb.net/?retryWrites=true&w=majority')
 
-
-app = Flask(__name__,template_folder='template')
+db = cl['Hospital']
+collection = db['Departments']
 
 
 @app.route('/')
 def index():
-    return render_template("index.html") 
+    data = list(collection.find({}))
+    return render_template("index.html",data=data)
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
