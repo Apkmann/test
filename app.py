b2ede13c0dc972bdfd195b3df49dc0990edafa9f
Mohammed Aswath L <102312905+Apkmann@users.noreply.github.com>
2023-10-07 13:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask ,request,render_template,redirect,url_for
 from pymongo import MongoClient
-import urllib.parse
 import certifi
 ca = certifi.where()
-passw = "Aswath22@data"
-p = urllib.parse.quote_plus(passw)
 app = Flask(__name__,template_folder='template',static_url_path="/static")
-cl = MongoClient(f'mongodb+srv://mohammedaswath141:{p}@cluster0.54i4ysr.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+cl = MongoClient('mongodb+srv://mohammedaswath141:Aswath22%40data@cluster0.54i4ysr.mongodb.net/Hospital?retryWrites=true&w=majority',tlsCAFile=ca)
 
 db = cl['Hospital']
 collection = db['Departments']
